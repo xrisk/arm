be759b20e985e048a68d2bb4383f85464cd51965
Rishav Kundu <xrisk@Rishavs-MacBook-Pro.local>
2019-04-02 18:44:27 +0530

    Add working code

--- final.py
+++ final.py
@@ -1,108 +1,79 @@
-import dxl
-import time
+from dxl2 import Motor, Connection, Instruction, MotorType
+from math import sqrt, acos, pi
+from time import sleep
+
 import math
-from whatup import *
-
-# coords
-# x = int(input("x:"))
-# y = int(input("y:"))
-
-x, y, fx, fy = (
-    udharja()
-)  # (x, y) are coordinates of where tthe object is. (fx, fy) for final position.
-print(x, y, fx, fy)
-a = dxl.get_available_ports()
-print(a)
-d = dxl.dxl(a[0], 1000000)
-print(d.scan(10))
 
 
-def ghum(
-    x, y, z, a, b
+conn = Connection("/dev/tty.usbserial-A5052MJ8")
+conn.open_port()
+
+m_upper = Motor(conn, 5, MotorType.AX)
+m_lower = Motor(conn, 4, MotorType.AX)
+m_pencil = Motor(conn, 6, MotorType.AX)
+
+m_upper.write(Instruction.MOVING_SPEED, 100)
+m_lower.write(Instruction.MOVING_SPEED, 100)
+
+m_upper.write(Instruction.TORQUE_ENABLE, 1)
+m_lower.write(Instruction.TORQUE_ENABLE, 1)
+
+PENCIL_UP = 608
+PENCIL_DOWN = 502
+
+
+def to_degree(x):
+    return x * 180 / pi
+
+def map(x, in_min, in_max, out_min, out_max):
+    ret = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return int(ret)
+
+def move_to(
+    x, y
 ):  # (x, y) are coords to move to. z is positon of arm either to be up or down. a is for the stationary motor. b is for gripper angle initially.
     # lengths
-    a2 = 10.5  # shoulder to elbow
-    a4 = 11.5  # elbow to wrist
+    l1 = 10 # shoulder to elbow
+    l2 = 14   # elbow to wrist
+    r = sqrt(x ** 2 + y ** 2)
 
-    if x <= 0:
-        x += 2
-        y += 2
-        # value of r
-        r = math.sqrt(pow(x, 2) + pow(y, 2))
-        # equation 1
-        pa = (pow(y, 2) + pow(r, 2) - pow(x, 2)) / (2 * y * r)
-        p1 = math.acos(pa) * 57.2958
-        # equation 2
-        pb = (pow(a2, 2) + pow(r, 2) - pow(a4, 2)) / (2 * a2 * r)
-        p2 = math.acos(pb) * 57.2958
-        # equation 3
-        pc = (pow(a4, 2) + pow(a2, 2) - pow(r, 2)) / (2 * a2 * a4)
-        p3 = math.acos(pc) * 57.2958
+    if r > (l1 + l2):
+        print("math error: ", r, l1 + l2)
+        return
 
-        # angles
-        m = p1 - p2
-        n = 180 - p3
+    theta_2 = pi - acos((l1 ** 2 + l2 ** 2 - r ** 2) / (2 * l1 * l2))
+    theta_1 = acos(x / r) - acos((l1 ** 2 + r ** 2 - l2 ** 2) / (2 * l1 * r))
 
-        # writing angles
-        m3 = int(512 + (m * 3.413))
-        m4 = int(512 + (n * 3.413))
-        m6 = 450
+    theta_1 = theta_1 - pi / 2
 
-    else:
-        if x > 1:
-            x -= 1
-        y -= 1
-        # value of rradius
-        r = math.sqrt(pow(x, 2) + pow(y, 2))
-        # equation 1
-        pa = (pow(a2, 2) + pow(r, 2) - pow(a4, 2)) / (2 * a2 * r)
-        p1 = math.acos(pa) * 57.2958
-        # equation 2
-        pb = (pow(x, 2) + pow(r, 2) - pow(y, 2)) / (2 * x * r)
-        p2 = math.acos(pb) * 57.2958
-        # equation 3
-        pc = (pow(a4, 2) + pow(a2, 2) - pow(r, 2)) / (2 * a2 * a4)
-        p3 = math.acos(pc) * 57.2958
+    theta_1 = to_degree(theta_1)
+    theta_2 = to_degree(theta_2)
 
-        # angles
-        m = p2 - p1
-        n = 180 - p3
+    print(f"theta_1: {theta_1}")
+    print(f"theta_2: {theta_2}")
 
-        # writing angles
-        m3 = int(211 + (m * 3.413))
-        m4 = int(512 + (n * 3.413))
-        m6 = 790
+    theta_2 = map(theta_2, -150, +150, 0, 1023)
+    theta_1 = map(theta_1, -150, +150, 0, 1023)
 
-    # dictionary writting values to motors corresponding to the motor ID
-    dictionary = {1: 2048, 2: a, 3: m3, 4: m4, 5: 202, 6: m6, 7: z}
-    # zero for 1 is in 2122 and zero position for 2 is in 2068
-    # zero for 3-7 are in 512
-    d.speed(1, 40)
-    d.speed(2, 40)
-    d.speed(3, 40)
-    d.speed(4, 40)
-    d.speed(5, 40)
-    d.speed(6, 40)
-    d.speed(7, 40)
+    m_upper.write(Instruction.GOAL_POSITION, theta_2))
+    m_lower.write(Instruction.GOAL_POSITION, theta_1))
 
-    # sync-write command
-    d.set_goal_position(dictionary)
-    time.sleep(5)
-    d.move(2, 2950)
-    time.sleep(4)
-    d.move(7, 450)
-    time.sleep(2)
-    d.move(7, b)
-    # for a time interval between commands use: time.sleep(seconds)
-
-    # setting torque to zero(not very necessary)
-    # d.set_torque({k: 0 for k in range(1, 8)})
-
-    # prints done after all commands are executed
-    print("done")
+# for x_start in rage()
+# move_to(0, 17)
 
 
-ghum(x, y, 700, 2122, 450)
-time.sleep(1)
-ghum(fx, fy, 450, 2122, 700)
+def y(x):
+    return x ** 2 + 12
 
+# print(m_pencil.write(Instruction.GOAL_POSITION, PENCIL_UP))
+# print(m_lower.write(Instruction.GOAL_POSITION, 800))
+# move_to(0, 24)
+# sleep(2)
+
+x_start = -2
+
+# m_pencil.write(Instruction.GOAL_POSITION, PENCIL_DOWN)
+while x_start <= +10:
+    move_to(x_start, y(x_start))
+    x_start += 0.05
+    sleep(1)
